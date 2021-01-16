3f877cbf80dd435cccbad18f5e02950ba17307f0
IgorFerro <igor.ferro.parreira@gmail.com>
2021-01-16 09:45:49 -0300

    Configure Virtual Users

--- tests/02-script.js
+++ tests/02-script.js
@@ -6,6 +6,12 @@
 
 import http from 'k6/http'
 
+export let options = {
+    //Declare configuration
+    vus:10,
+    duration:'1s'
+}
+
 export default function () {
     http.get('https://www.google.com/')
 }
