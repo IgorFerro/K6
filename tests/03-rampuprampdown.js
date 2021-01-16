1fed2aa2c45aaf3fd85e1e0e6dce8224dfe5c735
IgorFerro <igor.ferro.parreira@gmail.com>
2021-01-16 10:04:58 -0300

    Ramp-up Ramp-down User Load

--- tests/03-rampuprampdown.js
+++ tests/03-rampuprampdown.js
@@ -3,7 +3,7 @@ import http from 'k6/http'
 //Ramp up anda down users
 export let options = {
     stages: [
-        { duration: '1s', target: 20}, // 20 users for 1 seconds
+        { duration: '1s', target: 30}, // 30 users for 1 seconds
 
         { duration: '1s', target: 40}  // 40 users for 1 seconds
     ]
