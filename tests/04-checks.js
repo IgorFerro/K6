5dcfa25ae3f7a46b27803c4bdbd2033d67715a93
IgorFerro <igor.ferro.parreira@gmail.com>
2021-01-18 10:33:42 -0300

    Multiple Checks

--- tests/04-checks.js
+++ tests/04-checks.js
@@ -4,14 +4,26 @@ https://designer.mocky.io/
 
 https://run.mocky.io/v3/0dc12919-3623-41f2-b556-d68f3caa0a82
 
+Sencond API:
+https://run.mocky.io/v3/8e2f98e8-2525-4f9e-bf0d-5fc8744b4b78
+"Message": "API Executed Successfully"
+
 */
 
 import http from 'k6/http'
 import {check} from 'k6'
 
 export default function(){
-   let response = http.get('https://run.mocky.io/v3/0dc12919-3623-41f2-b556-d68f3caa0a82')
+   //let response = http.get('https://run.mocky.io/v3/0dc12919-3623-41f2-b556-d68f3caa0a82')
+
+   let response = http.get('https://run.mocky.io/v3/8e2f98e8-2525-4f9e-bf0d-5fc8744b4b78')
+
+    //Print Logs
+    console.log(`response body length ${response.body.length} for VU= ${__VU} ITERA = ${__ITER}`)
+
+
    check(response, {
        'is response status is 200 :' : (r) => r.status === 200,
+       'body size is 43 bytes :' : (r) => r.body.length == 39,
    })
 }
