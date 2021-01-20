64cdf392263103e9d8d1cf0aab5980d7c64b263a
IgorFerro <igor.ferro.parreira@gmail.com>
2021-01-20 07:28:24 -0300

    Understand Trend

--- tests/05-rate.js
+++ tests/05-rate.js
@@ -3,12 +3,19 @@ Checks will not fail the whote tests suite
 so we will use Rate along with check
 */
 
+import {Trend} from 'k6/metrics'
 import http from 'k6/http'
 import {check} from 'k6'
 import {Rate} from 'k6/metrics'
 
 //export variable that will use in test
 export let errorRate = new Rate('errors')
+
+//Define Trend
+var getApiTrend = new Trend("TREND_Get_Api_Duration")
+var getApiWaiting = new Trend("TREND_Get_Api_Waiting")
+var googleGetApiTrend = new Trend("TREND_Google_Get_Api_Duration")
+var googleGetApiWaiting = new Trend("TREND_Google_Get_Api_Waiting")
 
 //define options 
 export let options = {
@@ -30,5 +37,17 @@ export default function(){
    const check2 = check(response, {
    'body size is 43 bytes :' : (r) => r.body.length == 43,
   })
-  errorRate.add(check2); // !i.e not 200, body lenght not matched aso add noto operator
+  errorRate.add(!check2); // !i.e not 200, body lenght not matched aso add noto operator
+  
+  // Added response duration inside custom trend
+  getApiTrend.add(response.timings.duration)
+  getApiWaiting.add(response.timings.waiting)
+  
+
+  // Add one more API
+  const googleResponse = http.get('https://www.google.com/')
+  //Add Trend
+  googleGetApiTrend.add(googleResponse.timings.duration)
+  googleGetApiWaiting.add(googleResponse.timings.waiting)
+
   }
