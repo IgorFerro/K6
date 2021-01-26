72265fbabc9dda7c37af8fef43186624ab3faa72
IgorFerro <igor.ferro.parreira@gmail.com>
2021-01-26 07:24:42 -0300

    Threshold on Groups

--- tests/16-loadimpact-basics.js
+++ tests/16-loadimpact-basics.js
@@ -8,13 +8,18 @@ Basics loadimpact script
  //Groups
  Allows you to group load test k6 scripts for better visualization
 
+ //Threashold on groups
+ Defines pass/fail criteria for group
+
 */
 
 import http from 'k6/http';
 import {check,group} from 'k6'
-import {Rate} from 'k6/metrics'
+import {Rate,Trend} from 'k6/metrics'
+
 
 //Declare Variables
+let groupDuration = Trend("groupDuration")
 export let errorRate = new Rate('errors')
 
 //Define Options
@@ -22,7 +27,13 @@ export let options = {
     vus: 10,
     duration: '2s',
     thresholds: {
-        errors:['rate<0.1'] //10% errors
+        errors:['rate<0.1'], //10% errors
+
+        'groupDuration{groupName:groupGetUsers}':['avg < 200'],
+        'groupDuration{groupName:groupGetGroups}':['avg< 300'],
+
+        //Trend to measure group timing
+
     },
 
     ext: {
@@ -32,12 +43,27 @@ export let options = {
           name: "Trainning"
         }
       }
-}
+    }
+
+    // Function to measure Time
+    function groupWithMetrics(nameOfGroup, groupFunction ) {
+        //Start time
+        let start = new Date()
+        // Call Group
+        group(nameOfGroup, groupFunction);
+        //Stop Time
+        let end = new Date();
+        //Add Trend Timming
+        groupDuration.add(end-start, {groupName: nameOfGroup})
+       
+
+
+    }
 
     //Default main function -API 1
     export default function () {
 
-    group("groupGetUsers", function  (){ 
+        groupWithMetrics("groupGetUsers", function  (){ 
         const responseGetUsers = http.get('https://run.mocky.io/v3/d3cfd6eb-5088-43eb-b27a-0e690d870402');
         const checkGetUsers = check(responseGetUsers, {
             "is response os API1 is 200 : " : r => r.status ===200
@@ -47,7 +73,7 @@ export let options = {
        })
 
         //API- 2
-    group("groupGetGroups", function(){
+        groupWithMetrics("groupGetGroups", function(){
             const responseGetGroups = http.get('https://run.mocky.io/v3/bb4ac454-8307-46e2-9281-598c9c754121');
             const checkGetGroups= check(responseGetGroups, {
                 "is response of Get Groups is 200 : " : r => r.status ===200
