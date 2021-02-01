258d53bc8a18fb22adf36304ac667b3d2e4d5c1d
IgorFerro <igor.ferro.parreira@gmail.com>
2021-02-01 08:04:22 -0300

    Creste post request

--- framework/utility/apiService.js
+++ framework/utility/apiService.js
@@ -11,3 +11,18 @@ var createApiTrend = new Trend("Trend_CreateApi")
 var getApiTrend = new Trend("Trend_GetApi")
 var deleteApiTrend = new Trend("Trend_DeleteApi")
 
+//Define type header function
+export const setHeader = () => {
+    return {
+        headers :{
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+    }
+}
+
+export const route_createApi = (enpoint , token) => `${enpoint}?test${token}`
+
+export function createApi(endpoint, token) {
+    console.log(`Inside createApi token=${token}`)
+    let postResponse = http.post(`${route_createApi(endpoint,token)}`,null,setHeader())
+}
