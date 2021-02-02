69106ecbca5615a93e3ae7eaf37fac39926e5917
IgorFerro <igor.ferro.parreira@gmail.com>
2021-02-02 07:05:59 -0300

    Create Some Parameters

--- framework/utility/apiService.js
+++ framework/utility/apiService.js
@@ -25,4 +25,26 @@ export const route_createApi = (enpoint , token) => `${enpoint}?test${token}`
 export function createApi(endpoint, token) {
     console.log(`Inside createApi token=${token}`)
     let postResponse = http.post(`${route_createApi(endpoint,token)}`,null,setHeader())
+    //Add Trend
+    createApiTrend.add(postResponse.timings.duration)
+    //Check for 200
+    checkPostResponse = check(postResponse, {
+        "Create Api status 200" : r => r.status ===20
+    })
+    //Error Rate
+    failureRate.add(!checkPostResponse)
+    //Read the response
+    let responseBody = JSON.parse(postResponse.body)
+
+    try {
+        var id = responseBody[0].id;
+        console.log(`course id is ${id}`)
+    } catch (ex) {
+        checkPostResponse = check(postResponse,{
+            "Create Api does not return valid data ": r.status === 999
+        })
+        failureRate.add(!checkPostResponse)
+    }
+
+
 }
