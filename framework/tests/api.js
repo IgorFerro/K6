a0158bdb0f6a7593cc1d0699c228c8ad10148b1a
IgorFerro <igor.ferro.parreira@gmail.com>
2021-02-06 07:01:59 -0300

    Test Script

--- framework/tests/api.js
+++ framework/tests/api.js
@@ -21,3 +21,26 @@ else if(`${__ENV.ENVIRONMENT}` == env.dev){
     environment = env.devEnvironment;
     token = `${__ENV.DEV_TOKEN}`
 }
+
+// Test Life Cycle - Setup
+export function setUp() {
+    
+}
+
+// Test Life Cycle - Default function - VU Main function
+
+export default function () {
+    try {
+        //Call api workflow
+
+        let responseBody = apiService.createApi(`${environment.SERVER_ENPOINT}`,token);
+        apiService.route_getAPi(environment.SERVER_ENPOINT, token ,responseBody[0].id)
+        apiService.route_deleteApi(environment.SERVER_ENPOINT, token ,responseBody[0].id)
+        
+    } catch (ex) {
+        console.log(`error occurs in execution`)
+        
+    }
+}
+
+// Test Life Cycle - Default function - Clean Up
